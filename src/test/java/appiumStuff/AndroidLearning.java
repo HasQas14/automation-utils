52cfb599c653d5301f02c119bcfe095b28eda976
Qaseem Hasan <qaseem.hasan@paytm.com>
2025-05-25 16:34:53 +0530

    some appium learning stuff

---
+++ src/test/java/appiumStuff/AndroidLearning.java
@@ -0,0 +1,21 @@
+package appiumStuff;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AndroidLearning {
+
+    @Test
+    public void AppiumTest() throws MalformedURLException {
+
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName("Pixel 2 XL");
+//        options.setApp();
+
+//        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), null);
+    }
+}
